a4e34ca283baf606b7f19c660cbd40ffcbb2fd1b
djihane Ghilani <djihaneghilani@gmail.com>
2025-09-02 21:45:15 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by MAC on 2/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
